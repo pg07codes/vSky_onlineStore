08bc18507045fac018d1acf2154000c8ffa90074
pranav gupta <pg07codes@gmail.com>
2018-08-11 10:18:42 +0530

    rebased

--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -1,10 +1,14 @@
 /*created by Pranav Gupta (pg07codes) on 11-08-2018 */
 
 const route=require("express").Router()
+const item=require("../../db/models").item
 
 route.get("/",(r,s)=>{
     s.render("admin")
 })
+
+
+
 
 
 module.exports=route
